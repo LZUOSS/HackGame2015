243e455b59e44c923d07c5441067f46981b45afc
Bird <dstsmallbird@foxmail.com>
2015-10-12 00:53:58 +0000

    Fix bugs
    Signed-off-by: Bird <dstsmallbird@foxmail.com>

--- app/controllers/hackgame_controller.rb
+++ app/controllers/hackgame_controller.rb
@@ -5,16 +5,16 @@ class HackgameController < ApplicationController
 		password = params[:pwd]
 		authcode = params[:auth]
 		if level_id == "0" && password == "hahahahahaha"
-			redirect_to "http://localhost:3000/hackgame/1.html"
+			redirect_to "https://hackgame.cnbirdlink.com/hackgame/1.html"
 		elsif level_id == "1" && password == "password" && authcode == "00001234567"
-			redirect_to "http://localhost:3000/hackgame/2.html"
+			redirect_to "https://hackgame.cnbirdlink.com/hackgame/2.html"
 		elsif level_id == "2" && password == "nishigehaoren"
-			redirect_to "http://localhost:3000/hackgame/3.html"
+			redirect_to "https://hackgame.cnbirdlink.com/hackgame/3.html"
 		elsif level_id == "3" && password == "lalalalalala"
-			redirect_to "http://localhost:3000/hackgame/4.html"
+			redirect_to "https://hackgame.cnbirdlink.com/hackgame/4.html"
 			cookies[:level4_pwd] = "gongxigongxi"
 		elsif level_id == "4" && password == "gongxigongxi"
-			redirect_to "http://localhost:3000/hackgame/5.html"
+			redirect_to "https://hackgame.cnbirdlink.com/hackgame/5.html"
 		elsif level_id == "5" && password == "A401"
 			redirect_to "http://oss.lzu.edu.cn/2015/"
 		else
