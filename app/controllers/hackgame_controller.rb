c3156fe7f9e5d567ebeae11a0827ba5331eb1b3c
Bird LINK <Bird LINK>
2015-10-28 14:52:59 +0800

    New games
    Signed-off-by: Bird LINK <Bird LINK>

--- app/controllers/hackgame_controller.rb
+++ app/controllers/hackgame_controller.rb
@@ -1,25 +1,41 @@
-class HackgameController < ApplicationController
+class HackgameController < ApplicationController
 
 	def CheckPassword
 		level_id = params[:level]
 		password = params[:pwd]
 		authcode = params[:auth]
-		if level_id == "0" && password == "hahahahahaha"
+		if level_id == "0" && password == "huanyinglaidaohackgame"
 			redirect_to "https://hackgame.cnbirdlink.com/hackgame/1_xdaserqd.html"
-		elsif level_id == "1" && password == "A401"
+		elsif level_id == "1" && password == "zhouqingguo"
 			redirect_to "https://hackgame.cnbirdlink.com/hackgame/2-dfsff.html"
-		elsif level_id == "2" && password == "nishigehaoren"
+		elsif level_id == "2" && password == "duibuqinishigehaoren"
 			redirect_to "https://hackgame.cnbirdlink.com/hackgame/3-dsfad.html"
 		elsif level_id == "3" && password == "lalalalalala"
 			redirect_to "https://hackgame.cnbirdlink.com/hackgame/4-dsfas.html"
-			cookies[:level4_pwd] = "gongxigongxi"
-		elsif level_id == "4" && password == "gongxigongxi"
+			cookies[:level4_pwd] = "tingtianyouming..."
+		elsif level_id == "4" && password == "tingtianyouming..."
 			redirect_to "https://hackgame.cnbirdlink.com/hackgame/5_ereqfdsf.html"
-		elsif level_id == "5" && password == "password" && authcode == "00001234567"
-			redirect_to "http://oss.lzu.edu.cn/2015/"
+		elsif level_id == "5" && password == "320" && authcode == "23333333"
+			redirect_to "https://hackgame.cnbirdlink.com/hackgame/6_fasdcfac.html"
+		elsif level_id == "6" && password == 'zhouding+nan+sumiaoling'
+			redirect_to "https://hackgame.cnbirdlink.com/hackgame/7_fsdfas.html"
+		elsif level_id == "7" && password == "woaini"
+			redirect_to "https://hackgame.cnbirdlink.com/hackgame/8_dfafasd.html"	
+		elsif level_id == "8" && password == "2366666"
+			redirect_to "ttps://hackgame.cnbirdlink.com/hackgame/finish_game.html"
 		else
 			redirect_to request.env["HTTP_REFERER"]
 		end
 	end
 	
+	def GetPassword
+		password = params[:psw]
+		if password == '23333333'
+			format.html {  }
+        	render json: {result: {true}}
+        else
+        	format.html {  }
+        	render json: {result: {false}}
+        end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
     get 'ChkPassword' => 'hackgame#CheckPassword'
+    get 'GetPassowrd' => 'hackgame#GetPassowrd'
 end
